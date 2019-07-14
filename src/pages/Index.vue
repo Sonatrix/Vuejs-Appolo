baf0333808384c50555efc6957fb5c06f8a852c4
Sonatrix <praveenkumarverma121@gmail.com>
2019-07-15 00:13:54 +0530

    Initialise project

---
+++ src/pages/Index.vue
@@ -0,0 +1,43 @@
+<template>
+   <q-page class="flex flex-center">
+    <q-list highlight >
+      <q-list-header>Task</q-list-header>
+      <q-item tag="label" v-for="i in articles" :key="i.id">
+        <q-item-main>
+          <q-item-tile label>{{i.title}}</q-item-tile>
+          <q-item-tile sublabel>{{i.published_at}}</q-item-tile>
+        </q-item-main>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import gql from 'graphql-tag'
+const ArticlesQuery = gql`
+  query {
+    articles {
+      title
+      published_at
+      title
+    }
+  }
+`
+
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      // Initialize your apollo data
+      articles: []
+    }
+  },
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    articles: ArticlesQuery
+  }
+}
+</script>
